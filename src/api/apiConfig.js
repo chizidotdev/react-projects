66bd374726c1be4c6bf0206c4b9d98585f7d9526
chiziivictor <chizivwokoma@gmail.com>
2022-08-12 04:57:12 +0100

    modify api key

--- src/api/apiConfig.js
+++ src/api/apiConfig.js
@@ -1,8 +1,8 @@
 const apiConfig = {
-    baseUrl: 'https://api.themoviedb.org/3/',
-    apiKey: 'get from themoviedb.org',
-    originalImage: (imgPath) => `https://image.tmdb.org/t/p/original/${imgPath}`,
-    w500Image: (imgPath) => `https://image.tmdb.org/t/p/w500/${imgPath}`
-}
+  baseUrl: "https://api.themoviedb.org/3/",
+  apiKey: "ee699edd5b9cb5bea4900059932aeaee",
+  originalImage: (imgPath) => `https://image.tmdb.org/t/p/original/${imgPath}`,
+  w500Image: (imgPath) => `https://image.tmdb.org/t/p/w500/${imgPath}`,
+};
 
 export default apiConfig;
